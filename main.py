45678b4a6f610f801aa2699d036680d7f885308c
Sim <sensei.boi@mail.ru>
2025-01-06 14:44:00 +0300

    SQL update

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ from db import add_entry
 
 def main(database):
     db.initiate(database)
-    #add_entry(database,"Journal", {'time':'06.01.2025:12.39', 'color' : 'red', 'license_number' : 'A421TH61','type_auto':'truck'})
-    res=db.select_data(database,"Journal")
-    print(res)
+
 
 
 if __name__ == "__main__":
